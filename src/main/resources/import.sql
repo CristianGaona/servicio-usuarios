5611e7a6458ec4aaba8662643e4f8913012c4183
CristianGaona <crgaonas24@gmail.com>
2020-02-09 14:07:19 -0500

    Version1 microservicio usuario

---
+++ src/main/resources/import.sql
@@ -0,0 +1,19 @@
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('crisda', '12345', 1, 'Cristian', 'Gaona', 'crgaonas24@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('sebas', '12345', 1, 'Sebastian', 'Gaona', 'asgaonac21@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('manu22', '12345', 1, 'Manuel', 'Ruiz', 'maruiz22@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('majo23', '12345', 1, 'María', 'Sanchez', 'masanchez23@gmail.com');
+INSERT INTO `usuarios` (username, password, enabled, nombre, apellido, email) VALUES ('andre25', '12345', 1, 'Andrea', 'Cruz', 'ancruz25@gmail.com');
+
+INSERT INTO `roles` (nombre) VALUES ('ROL_ADMIN');
+INSERT INTO `roles` (nombre) VALUES ('ROL_SECETARIO');
+INSERT INTO `roles` (nombre) VALUES ('ROL_GERENTE');
+INSERT INTO `roles` (nombre) VALUES ('ROL_MECANICO');
+INSERT INTO `roles` (nombre) VALUES ('ROL_VENDEDOR');
+INSERT INTO `roles` (nombre) VALUES ('ROL_RRHH');
+
+INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (1, 1);
+INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (2, 2);
+INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (1, 3);
+INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (4, 4);
+INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (5, 5);
+INSERT INTO `usuarios_roles` (usuario_id, rol_id) VALUES (3, 6);
