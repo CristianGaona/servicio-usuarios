7d6b3c3e0f6451ac24a4d0d410a45faeef6a95ed
CristianGaona <crgaonas24@gmail.com>
2020-02-19 04:33:09 -0500

    Agregado variable de correo a usuarios

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO `usuarios` (username,   nombre, apellido, url) VALUES ('crisda', 'Cristian', 'Gaona',  'https://image.flaticon.com/icons/png/512/401/401156.png');
-INSERT INTO `usuarios` (username,   nombre, apellido,  url) VALUES ('sebas', 'Sebastian', 'Gaona',  'https://image.flaticon.com/icons/png/512/1503/1503126.png');
-INSERT INTO `usuarios` (username,   nombre, apellido,  url) VALUES ('manu22', 'Manuel', 'Ruiz',  'https://image.flaticon.com/icons/png/512/1503/1503126.png');
-INSERT INTO `usuarios` (username,   nombre, apellido,  url) VALUES ('majo23',  'María', 'Sanchez',  'https://image.flaticon.com/icons/png/512/1503/1503126.png');
-INSERT INTO `usuarios` (username,   nombre, apellido,  url) VALUES ('andre25','Andrea', 'Cruz',  'https://image.flaticon.com/icons/png/512/1503/1503126.png');
+INSERT INTO `usuarios` (username,   nombre, apellido, email, url) VALUES ('crisda', 'Cristian', 'Gaona', 'crgaonas24@gmail.com', 'https://image.flaticon.com/icons/png/512/401/401156.png');
+INSERT INTO `usuarios` (username,   nombre, apellido, email, url) VALUES ('sebas', 'Sebastian', 'Gaona', 'asgaonac21@gmail.com', 'https://image.flaticon.com/icons/png/512/1503/1503126.png');
+INSERT INTO `usuarios` (username,   nombre, apellido, email, url) VALUES ('manu22', 'Manuel', 'Ruiz', 'maruiz22@gmail.com', 'https://image.flaticon.com/icons/png/512/1503/1503126.png');
+INSERT INTO `usuarios` (username,   nombre, apellido, email, url) VALUES ('majo23',  'María', 'Sanchez', 'masanchez23@gmail.com', 'https://image.flaticon.com/icons/png/512/1503/1503126.png');
+INSERT INTO `usuarios` (username,   nombre, apellido, email, url) VALUES ('andre25','Andrea', 'Cruz', 'ancruz25@gmail.com', 'https://image.flaticon.com/icons/png/512/1503/1503126.png');
